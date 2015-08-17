0917c395dd4cd5468fde2f556026b7a55a0f1c74
h82258652 <h82258652@hotmail.com>
2015-08-17 22:24:37 +0800

    完成委托命令

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/RelayCommand.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SoftwareKobo.UniversalToolkit.Mvvm
+{
+    /// <summary>
+    /// 委托命令。
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private readonly Func<Task> _asyncExecute;
+        private readonly Func<bool> _canExecute;
+        private readonly Action _execute;
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="execute">命令动作。</param>
+        public RelayCommand(Action execute) : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="asyncExecute">命令动作。</param>
+        public RelayCommand(Func<Task> asyncExecute) : this(asyncExecute, null)
+        {
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="execute">命令动作。</param>
+        /// <param name="canExecute">指示命令在某个状态下是否能够执行。</param>
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="asyncExecute">命令动作。</param>
+        /// <param name="canExecute">指示命令在某个状态下是否能够执行。</param>
+        public RelayCommand(Func<Task> asyncExecute, Func<bool> canExecute)
+        {
+            if (asyncExecute == null)
+            {
+                throw new ArgumentNullException(nameof(asyncExecute));
+            }
+
+            this._asyncExecute = asyncExecute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// 当出现影响是否应执行该命令的更改时发生。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// 指示当前状态是否允许命令执行。
+        /// </summary>
+        /// <param name="parameter">参数。</param>
+        /// <returns>是否允许命令执行。</returns>
+        public bool CanExecute(object parameter)
+        {
+            return this._canExecute == null || this._canExecute();
+        }
+
+        /// <summary>
+        /// 执行命令。
+        /// </summary>
+        /// <param name="parameter">参数。</param>
+        public async void Execute(object parameter)
+        {
+            if (this._execute != null)
+            {
+                this._execute();
+            }
+            else
+            {
+                await this._asyncExecute();
+            }
+        }
+
+        /// <summary>
+        /// 通知命令是否允许执行发生变更。
+        /// </summary>
+        public virtual void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/RelayCommandGeneric.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SoftwareKobo.UniversalToolkit.Mvvm
+{
+    /// <summary>
+    /// 委托命令。
+    /// </summary>
+    /// <typeparam name="T">参数类型。</typeparam>
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Func<T, Task> _asyncExecute;
+        private readonly Func<T, bool> _canExecute;
+        private readonly Action<T> _execute;
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="execute">命令动作。</param>
+        public RelayCommand(Action<T> execute) : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="asyncExecute">命令动作。</param>
+        public RelayCommand(Func<T, Task> asyncExecute) : this(asyncExecute, null)
+        {
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="execute">命令动作。</param>
+        /// <param name="canExecute">指示命令在某个状态下是否能够执行。</param>
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// 初始化委托命令。
+        /// </summary>
+        /// <param name="asyncExecute">命令动作。</param>
+        /// <param name="canExecute">指示命令在某个状态下是否能够执行。</param>
+        public RelayCommand(Func<T, Task> asyncExecute, Func<T, bool> canExecute)
+        {
+            if (asyncExecute == null)
+            {
+                throw new ArgumentNullException(nameof(asyncExecute));
+            }
+
+            this._asyncExecute = asyncExecute;
+            this._canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// 当出现影响是否应执行该命令的更改时发生。
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
+
+        /// <summary>
+        /// 指示当前状态是否允许命令执行。
+        /// </summary>
+        /// <param name="parameter">参数。</param>
+        /// <returns>是否允许命令执行。</returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || this._canExecute((T)parameter);
+        }
+
+        /// <summary>
+        /// 执行命令。
+        /// </summary>
+        /// <param name="parameter">参数。</param>
+        public async void Execute(object parameter)
+        {
+            if (this._execute != null)
+            {
+                _execute((T)parameter);
+            }
+            else
+            {
+                await _asyncExecute((T)parameter);
+            }
+        }
+
+        /// <summary>
+        /// 通知命令是否允许执行发生变更。
+        /// </summary>
+        public virtual void RaiseCanExecuteChanged()
+        {
+            this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
