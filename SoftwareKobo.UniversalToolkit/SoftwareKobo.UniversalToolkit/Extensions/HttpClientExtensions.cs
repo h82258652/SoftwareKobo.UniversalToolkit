c589ca7f722dd716303ceae67fc1f276c145d46e
h82258652 <h82258652@hotmail.com>
2015-08-17 22:57:00 +0800

    添加注释

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Extensions/HttpClientExtensions.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Extensions/HttpClientExtensions.cs
@@ -5,8 +5,18 @@ using Windows.Web.Http;
 
 namespace SoftwareKobo.UniversalToolkit.Extensions
 {
+    /// <summary>
+    /// HttpClient 扩展类。
+    /// </summary>
     public static class HttpClientExtensions
     {
+        /// <summary>
+        /// 获取 json 并反序列化为实体。
+        /// </summary>
+        /// <typeparam name="T">实体类型。</typeparam>
+        /// <param name="client">httpclient。</param>
+        /// <param name="uri">请求的 Uri。</param>
+        /// <returns>实体对象。</returns>
         public static async Task<T> GetJsonAsync<T>(this HttpClient client, Uri uri)
         {
             string json = await client.GetStringAsync(uri);
