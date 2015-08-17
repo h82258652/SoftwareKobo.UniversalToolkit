a71b61a0aa446916df0ac8f4c74852c1b96a807c
h82258652 <h82258652@hotmail.com>
2015-08-17 22:51:51 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Threading.Tasks;
+using Windows.Web.Http;
+
+namespace SoftwareKobo.UniversalToolkit.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<T> GetJsonAsync<T>(this HttpClient client, Uri uri)
+        {
+            string json = await client.GetStringAsync(uri);
+            return await Task.Factory.StartNew(() => JsonConvert.DeserializeObject<T>(json));
+        }
+    }
+}
