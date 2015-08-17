4cef7d7653f935da8cfa1956c9f9e246d127037c
h82258652 <h82258652@hotmail.com>
2015-08-17 22:27:17 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using Windows.ApplicationModel;
+
+namespace SoftwareKobo.UniversalToolkit.Mvvm
+{
+    /// <summary>
+    /// 视图模型基类。
+    /// </summary>
+    public class ViewModelBase : BindableBase
+    {
+        /// <summary>
+        /// 指示当前是否处于设计模式。
+        /// </summary>
+        public static bool IsInDesignMode
+        {
+            get
+            {
+                return DesignMode.DesignModeEnabled;
+            }
+        }
+    }
+}
