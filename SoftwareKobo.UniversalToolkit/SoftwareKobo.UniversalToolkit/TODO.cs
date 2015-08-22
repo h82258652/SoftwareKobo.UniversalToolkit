07520906c1708af5020dfd4405a59d7355d32df9
h82258652 <h82258652@hotmail.com>
2015-08-22 10:25:44 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/TODO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftwareKobo.UniversalToolkit
+{
+    // WrapPanel
+    // DockPanel
+    // AeroPanel
+    // 瀑布流面板
+    // 汉堡按钮（绑定SplitView用于开关）
+    // 下拉刷新面板
+    // CircleImage
+
+    // 图片缓存系统
+
+    // ValidationSystem
+
+    // Kinds of triggers
+
+    // 后台任务helper or system（在Bootstrapper自动注册管理）
+
+    // ListViewBaseExtensions
+
+    // 改进 Bootstrapper，特别是 NavigationState
+}
---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Triggers/OrientationStateTrigger.cs
@@ -0,0 +1,39 @@
+﻿using Windows.Graphics.Display;
+using Windows.UI.Xaml;
+
+namespace SoftwareKobo.UniversalToolkit.Triggers
+{
+    public class OrientationStateTrigger : StateTriggerBase
+    {
+        public OrientationStateTrigger()
+        {
+            DisplayInformation.GetForCurrentView().OrientationChanged += OrientationStateTrigger_OrientationChanged;
+        }
+
+        private void OrientationStateTrigger_OrientationChanged(DisplayInformation sender, object args)
+        {
+            this.SetActive(sender.CurrentOrientation == Orientation);
+        }
+
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(DisplayOrientations), typeof(OrientationStateTrigger), new PropertyMetadata(DisplayOrientations.None, OrientationChanged));
+
+        private static void OrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = (OrientationStateTrigger)d;
+            var value = (DisplayOrientations)e.NewValue;
+            obj.SetActive(value == obj.Orientation);
+        }
+
+        public DisplayOrientations Orientation
+        {
+            get
+            {
+                return (DisplayOrientations)this.GetValue(OrientationProperty);
+            }
+            set
+            {
+                this.SetValue(OrientationProperty, value);
+            }
+        }
+    }
+}
