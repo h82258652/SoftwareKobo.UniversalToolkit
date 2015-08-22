c5fdafe307d880a49858f95cdece7a252a1b294e
h82258652 <h82258652@hotmail.com>
2015-08-22 10:39:32 +0800

    update

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/TODO.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/TODO.cs
@@ -8,8 +8,8 @@ namespace SoftwareKobo.UniversalToolkit
 {
     // WrapPanel
     // DockPanel
-    // AeroPanel
-    // 瀑布流面板
+    // AeroPanel https://github.com/JustinXinLiu/EffectsAndAnimationsWinComposition
+    // 瀑布流面板 http://www.cnblogs.com/ms-uap/p/4715195.html
     // 汉堡按钮（绑定SplitView用于开关）
     // 下拉刷新面板
     // CircleImage
@@ -25,4 +25,14 @@ namespace SoftwareKobo.UniversalToolkit
     // ListViewBaseExtensions
 
     // 改进 Bootstrapper，特别是 NavigationState
+
+    // 多窗口管理 http://www.cnblogs.com/tcjiaan/p/4748697.html
+
+    // 异步方法异常捕捉 http://www.cnblogs.com/youngytj/p/4749004.html
+
+    // 内置分享改进 http://www.cnblogs.com/ms-uap/p/4242326.html
+
+    // 模板选择器 http://www.cnblogs.com/ms-uap/p/4201334.html
+
+    // 封装获取屏幕分辨率 http://www.cnblogs.com/hebeiDGL/p/4609672.html
 }
