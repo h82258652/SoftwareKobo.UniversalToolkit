16e9ed6765f5a27a918c8f247fca2a2dc83c8144
h82258652 <h82258652@hotmail.com>
2015-08-24 17:00:47 +0800

    update

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Storage/SettingsStrategy.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Storage/SettingsStrategy.cs
@@ -1,9 +1,23 @@
 ﻿namespace SoftwareKobo.UniversalToolkit.Storage
 {
+    /// <summary>
+    /// 设置存储策略。
+    /// </summary>
     public enum SettingsStrategy
     {
+        /// <summary>
+        /// 本地设置。
+        /// </summary>
         Local,
+
+        /// <summary>
+        /// 漫游设置。
+        /// </summary>
         Roaming,
+
+        /// <summary>
+        /// 临时设置。
+        /// </summary>
         Temp,
     }
 }
