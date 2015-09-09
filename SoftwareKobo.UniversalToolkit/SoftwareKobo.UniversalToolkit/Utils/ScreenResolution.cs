f214bb1f1cf2c67ebc570f3c1728b8e844619adc
h82258652 <h82258652@hotmail.com>
2015-09-09 21:29:38 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Utils/ScreenResolution.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using Windows.Devices.Input;
+using Windows.Graphics.Display;
+
+namespace SoftwareKobo.UniversalToolkit.Utils
+{
+    /// <summary>
+    /// 屏幕分辨率。
+    /// </summary>
+    public static class ScreenResolution
+    {
+        /// <summary>
+        /// 获取屏幕高度。
+        /// </summary>
+        public static int Height
+        {
+            get
+            {
+                var rect = PointerDevice.GetPointerDevices().Last().ScreenRect;
+                var scale = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+                return (int)(rect.Height * scale);
+            }
+        }
+
+        /// <summary>
+        /// 获取屏幕宽度。
+        /// </summary>
+        public static int Width
+        {
+            get
+            {
+                var rect = PointerDevice.GetPointerDevices().Last().ScreenRect;
+                var scale = DisplayInformation.GetForCurrentView().RawPixelsPerViewPixel;
+                return (int)(rect.Width * scale);
+            }
+        }
+    }
+}
--- SoftwareKobo.UniversalToolkit/Test/MainPage.xaml.cs
+++ SoftwareKobo.UniversalToolkit/Test/MainPage.xaml.cs
@@ -1,9 +1,11 @@
-﻿using SoftwareKobo.UniversalToolkit.Storage;
+﻿using Newtonsoft.Json;
+using SoftwareKobo.UniversalToolkit.Storage;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 using Windows.Foundation;
@@ -35,34 +37,6 @@ namespace Test
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Uri uri;
-            try
-            {
-                uri = new Uri(txt.Text);
-            }
-            catch 
-            {
-                return;
-            }
-            var b = new StorageCachedImage(uri);
-            b.ImageFailed += B_ImageFailed;
-            img.Source = b;
-     //       img.Source = new SoftwareKobo.UniversalToolkit.Storage.StorageCachedImage(uri);            
-        }
-
-        private void B_ImageFailed(object sender, ImageLoadFailedEventArgs e)
-        {
-        }
-
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            long size = SoftwareKobo.UniversalToolkit.Storage.StorageCachedImage.GetCachedImagesSize();
-            await new MessageDialog(size.ToString()).ShowAsync();
-        }
-
-        private async void img_ImageFailed(object sender, ExceptionRoutedEventArgs e)
-        {
-            await new MessageDialog("failed").ShowAsync();
         }
     }
 }
