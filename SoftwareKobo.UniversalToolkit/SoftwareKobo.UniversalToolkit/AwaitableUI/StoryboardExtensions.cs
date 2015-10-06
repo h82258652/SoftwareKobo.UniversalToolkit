b27e0adb50e6aa2c658fe1ec251c3cd8de74e6f6
h82258652 <h82258652@hotmail.com>
2015-10-06 18:43:38 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/AwaitableUI/StoryboardExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace SoftwareKobo.UniversalToolkit.AwaitableUI
+{
+    public static class StoryboardExtensions
+    {
+        public static async Task BeginAsync(this Storyboard storyboard)
+        {
+            if (storyboard == null)
+            {
+                throw new ArgumentNullException(nameof(storyboard));
+            }
+
+            await EventAsync.FromEvent<object>(eh => storyboard.Completed += eh, eh => storyboard.Completed -= eh, storyboard.Begin);
+        }
+    }
+}
