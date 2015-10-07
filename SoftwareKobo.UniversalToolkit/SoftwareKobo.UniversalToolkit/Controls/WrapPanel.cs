976a3ad2ceaea0a4e6d459fe958d2e45d575a588
h82258652 <h82258652@hotmail.com>
2015-10-07 18:34:11 +0800

    working on it

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Controls/WrapPanel.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Controls/WrapPanel.cs
@@ -1,19 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.Foundation;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
 namespace SoftwareKobo.UniversalToolkit.Controls
 {
     public class WrapPanel : Panel
     {
+        public static readonly DependencyProperty ItemHeightProperty = DependencyProperty.Register(nameof(ItemHeight), typeof(double), typeof(WrapPanel), new PropertyMetadata(double.NaN, ItemWidthOrHeightChanged));
+
+        public static readonly DependencyProperty ItemWidthProperty = DependencyProperty.Register(nameof(ItemWidth), typeof(double), typeof(WrapPanel), new PropertyMetadata(double.NaN, ItemWidthOrHeightChanged));
+
+        public static readonly DependencyProperty OrientationProperty = DependencyProperty.Register(nameof(Orientation), typeof(Orientation), typeof(WrapPanel), new PropertyMetadata(Orientation.Horizontal, OrientationChanged));
+
+        public double ItemHeight
+        {
+            get
+            {
+                return (double)this.GetValue(ItemHeightProperty);
+            }
+            set
+            {
+                this.SetValue(ItemHeightProperty, value);
+            }
+        }
+
+        public double ItemWidth
+        {
+            get
+            {
+                return (double)this.GetValue(ItemWidthProperty);
+            }
+            set
+            {
+                this.SetValue(ItemWidthProperty, value);
+            }
+        }
+
         public Orientation Orientation
         {
-            get;
-            set;
+            get
+            {
+                return (Orientation)this.GetValue(OrientationProperty);
+            }
+            set
+            {
+                this.SetValue(OrientationProperty, value);
+            }
         }
 
         protected override Size ArrangeOverride(Size finalSize)
@@ -24,6 +57,37 @@ namespace SoftwareKobo.UniversalToolkit.Controls
         protected override Size MeasureOverride(Size availableSize)
         {
             return base.MeasureOverride(availableSize);
+        }
+
+        private static bool IsItemWidthHeightValid(double value)
+        {
+            return double.IsNaN(value) || (value >= 0.0d && double.IsPositiveInfinity(value) == false);
+        }
+
+        private static void ItemWidthOrHeightChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WrapPanel obj = (WrapPanel)d;
+            double value = (double)e.NewValue;
+
+            if (IsItemWidthHeightValid(value) == false)
+            {
+                throw new ArgumentException("Invalid item width or height", nameof(value));
+            }
+
+            obj.InvalidateMeasure();
+        }
+
+        private static void OrientationChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WrapPanel obj = (WrapPanel)d;
+            Orientation value = (Orientation)e.NewValue;
+
+            if (Enum.IsDefined(typeof(Orientation), value) == false)
+            {
+                throw new ArgumentException($"{nameof(Orientation)} is not defined", nameof(value));
+            }
+
+            obj.InvalidateMeasure();
         }
     }
 }
