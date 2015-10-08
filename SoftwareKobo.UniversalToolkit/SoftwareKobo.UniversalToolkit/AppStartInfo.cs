c982791de696fca7e9c9cb497bfcbb37fbc6cbde
h82258652 <h82258652@hotmail.com>
2015-10-08 20:48:01 +0800

    add comment

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/AppStartInfo.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/AppStartInfo.cs
@@ -11,18 +11,29 @@ namespace SoftwareKobo.UniversalToolkit
         {
         }
 
+        /// <summary>
+        /// 该启动方式使用的扩展启动屏幕。
+        /// </summary>
+        /// <remarks>若设置为 null 或者方法返回 null，则不使用扩展启动屏幕。</remarks>
         public Func<ExtendedSplashScreenContent> ExtendedSplashScreen
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// 是否使用一个新窗口展示内容（默认为 false）。
+        /// </summary>
         public bool IsShowInNewWindow
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// 该启动方式导航到哪个页面。
+        /// </summary>
+        /// <remarks>若设置为 null，则不对当前窗口进行导航。</remarks>
         public Type MainPage
         {
             get
@@ -36,6 +47,9 @@ namespace SoftwareKobo.UniversalToolkit
             }
         }
 
+        /// <summary>
+        /// 导航参数。
+        /// </summary>
         public object Parameter
         {
             get;
