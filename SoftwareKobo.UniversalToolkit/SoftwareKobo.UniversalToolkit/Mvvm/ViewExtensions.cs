5cff4b69d06f526e49498faeeefb31b3d9156ff1
h82258652 <h82258652@hotmail.com>
2015-10-09 22:08:04 +0800

    update

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewExtensions.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewExtensions.cs
@@ -1,11 +1,22 @@
-﻿using Windows.UI.Xaml;
+﻿using System;
+using Windows.UI.Xaml;
 
 namespace SoftwareKobo.UniversalToolkit.Mvvm
 {
     public static class ViewExtensions
     {
+        /// <summary>
+        /// 发送消息到对应的 ViewModel。
+        /// </summary>
+        /// <param name="view">需要发送消息的 View。</param>
+        /// <param name="parameter">消息内容。</param>
         public static void SendToViewModel(this FrameworkElement view, object parameter)
         {
+            if (view == null)
+            {
+                throw new ArgumentNullException(nameof(view));
+            }
+
             Messenger.Process(view, parameter);
         }
     }
--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewModelBase.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewModelBase.cs
@@ -29,10 +29,19 @@ namespace SoftwareKobo.UniversalToolkit.Mvvm
             Messenger.Unregister(this);
         }
 
+        /// <summary>
+        /// 接收来自 View 的消息。
+        /// </summary>
+        /// <param name="originSourceView">发送消息的 View。</param>
+        /// <param name="parameter">消息内容。</param>
         protected internal virtual void ReceiveFromView(FrameworkElement originSourceView, object parameter)
         {
         }
 
+        /// <summary>
+        /// 发送消息到对应的 View。
+        /// </summary>
+        /// <param name="parameter">消息内容。</param>
         protected void SendToView(object parameter)
         {
             Messenger.Process(this, parameter);
