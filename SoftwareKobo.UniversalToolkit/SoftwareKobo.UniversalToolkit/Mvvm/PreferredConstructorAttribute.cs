e80d6203d7d65d36d8e8f644565f3d03db155686
h82258652 <h82258652@hotmail.com>
2015-10-16 18:47:27 +0800

    添加简易IoC，添加ViewModelLocatorBase

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/PreferredConstructorAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SoftwareKobo.UniversalToolkit.Mvvm
+{
+    /// <summary>
+    /// 标注该 Attribute 在构造函数上，以指示 IoC 容器在创建对象时使用哪个构造函数。
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Constructor, Inherited = false, AllowMultiple = false)]
+    public sealed class PreferredConstructorAttribute : Attribute
+    {
+    }
+}
--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewModelLocatorBase.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Mvvm/ViewModelLocatorBase.cs
@@ -1,12 +1,39 @@
-﻿using System;
-
-namespace SoftwareKobo.UniversalToolkit.Mvvm
+﻿namespace SoftwareKobo.UniversalToolkit.Mvvm
 {
     /// <summary>
     /// 视图模型定位器基类。
     /// </summary>
-    [Obsolete]
     public abstract class ViewModelLocatorBase
     {
+        private IoC _container = new IoC();
+
+        /// <summary>
+        /// 获取注入类的实例。
+        /// </summary>
+        /// <typeparam name="T">注入类类型。</typeparam>
+        /// <returns>实例。</returns>
+        protected virtual T GetInstance<T>() where T : class
+        {
+            return this._container.GetInstance<T>();
+        }
+
+        /// <summary>
+        /// 注册注入类。
+        /// </summary>
+        /// <typeparam name="T">注入类类型。</typeparam>
+        protected virtual void Register<T>() where T : class
+        {
+            this._container.Register<T>();
+        }
+
+        /// <summary>
+        /// 注册注入类。
+        /// </summary>
+        /// <typeparam name="TFrom">注入类基类或实现的接口。</typeparam>
+        /// <typeparam name="TTo">注入类类型。</typeparam>
+        protected virtual void Register<TFrom, TTo>() where TTo : class, TFrom where TFrom : class
+        {
+            this._container.Register<TFrom, TTo>();
+        }
     }
 }
