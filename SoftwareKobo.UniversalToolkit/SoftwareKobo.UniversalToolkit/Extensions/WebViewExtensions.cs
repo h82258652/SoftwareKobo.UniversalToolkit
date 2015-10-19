8cd716487f698515a1e184967949b5636f57bfa6
h82258652 <h82258652@hotmail.com>
2015-10-19 23:04:20 +0800

    update

---
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Extensions/WebViewExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace SoftwareKobo.UniversalToolkit.Extensions
+{
+    public class WebViewExtensions
+    {
+        public static readonly DependencyProperty HtmlProperty = DependencyProperty.RegisterAttached("Html", typeof(string), typeof(WebViewExtensions), new PropertyMetadata(null, HtmlChanged));
+
+        public static string GetHtml(WebView obj)
+        {
+            return (string)obj.GetValue(HtmlProperty);
+        }
+
+        public static void SetHtml(WebView obj, string value)
+        {
+            obj.SetValue(HtmlProperty, value);
+        }
+
+        private static void HtmlChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            WebView webView = (WebView)d;
+            string value = (string)e.NewValue;
+
+            webView.NavigateToString(value);
+        }
+    }
+}
