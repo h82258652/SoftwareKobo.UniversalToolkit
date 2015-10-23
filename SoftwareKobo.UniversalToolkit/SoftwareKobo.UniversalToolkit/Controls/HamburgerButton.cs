65d44e0079728016fd9a5e8282d4f963628d03cd
h82258652 <h82258652@hotmail.com>
2015-10-23 21:24:26 +0800

    修改汉堡按钮

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Controls/HamburgerButton.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Controls/HamburgerButton.cs
@@ -5,11 +5,25 @@ namespace SoftwareKobo.UniversalToolkit.Controls
 {
     public sealed class HamburgerButton : ToggleButton
     {
+        public static readonly DependencyProperty ContentMarginProperty = DependencyProperty.Register(nameof(ContentMargin), typeof(Thickness), typeof(HamburgerButton), new PropertyMetadata(new Thickness()));
+
         public HamburgerButton()
         {
             this.DefaultStyleKey = typeof(HamburgerButton);
             this.Checked += this.HamburgerButton_Checked;
             this.Unchecked += this.HamburgerButton_Unchecked;
+        }
+
+        public Thickness ContentMargin
+        {
+            get
+            {
+                return (Thickness)this.GetValue(ContentMarginProperty);
+            }
+            set
+            {
+                this.SetValue(ContentMarginProperty, value);
+            }
         }
 
         private void HamburgerButton_Checked(object sender, RoutedEventArgs e)
