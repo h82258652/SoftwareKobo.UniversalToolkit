f0bdf02918733e4b1b6fc54e1166797ffb3bb026
h82258652 <h82258652@hotmail.com>
2015-11-11 18:24:43 +0800

    update

--- SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Helpers/NavigationBackHelper.cs
+++ SoftwareKobo.UniversalToolkit/SoftwareKobo.UniversalToolkit/Helpers/NavigationBackHelper.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.Phone.UI.Input;
 using Windows.UI.Core;
+using Windows.UI.Input;
 using Windows.UI.Xaml.Controls;
 
 namespace SoftwareKobo.UniversalToolkit.Helpers
@@ -10,13 +12,18 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
     public static class NavigationBackHelper
     {
         private static readonly Dictionary<Frame, EventHandler<BackPressedEventArgs>> _backPressedHandlers = new Dictionary<Frame, EventHandler<BackPressedEventArgs>>();
+
         private static readonly Dictionary<Frame, EventHandler<BackRequestedEventArgs>> _backRequestedHandlers = new Dictionary<Frame, EventHandler<BackRequestedEventArgs>>();
+
+        private static readonly Dictionary<Frame, CoreWindow> _frameWindows = new Dictionary<Frame, CoreWindow>();
+
+        private static readonly Dictionary<Frame, TypedEventHandler<CoreWindow, PointerEventArgs>> _pointerReleasedHandlers = new Dictionary<Frame, TypedEventHandler<CoreWindow, PointerEventArgs>>();
 
         public static void RegisterNavigateBack(this Frame frame, Func<Task> asyncAction)
         {
+            #region 标题栏后退键
             SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
             systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-
             if (_backPressedHandlers.ContainsKey(frame) == false)
             {
                 EventHandler<BackRequestedEventArgs> backRequestedHandler = async (sender, e) =>
@@ -30,7 +37,9 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
                 _backRequestedHandlers.Add(frame, backRequestedHandler);
                 systemNavigationManager.BackRequested += backRequestedHandler;
             }
+            #endregion
 
+            #region 设备后退键
             if (HardwareButtonsHelper.IsUseable && _backPressedHandlers.ContainsKey(frame) == false)
             {
                 EventHandler<BackPressedEventArgs> backPressedHandler = async (sender, e) =>
@@ -44,13 +53,35 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
                 _backPressedHandlers.Add(frame, backPressedHandler);
                 HardwareButtons.BackPressed += backPressedHandler;
             }
+            #endregion
+
+            #region 鼠标后退键
+            CoreWindow window = CoreWindow.GetForCurrentThread();
+            if (_frameWindows.ContainsKey(frame) == false && _pointerReleasedHandlers.ContainsKey(frame) == false)
+            {
+                TypedEventHandler<CoreWindow, PointerEventArgs> pointerReleasedHandler = async (sender, e) =>
+                {
+                    e.Handled = true;
+                    if (e.CurrentPoint.Properties.PointerUpdateKind == PointerUpdateKind.XButton1Released)
+                    {
+                        if (asyncAction != null)
+                        {
+                            await asyncAction.Invoke();
+                        }
+                    }
+                };
+                _frameWindows.Add(frame, window);
+                _pointerReleasedHandlers.Add(frame, pointerReleasedHandler);
+                window.PointerReleased += pointerReleasedHandler;
+            }
+            #endregion
         }
 
         public static void RegisterNavigateBack(this Frame frame, Action action)
         {
+            #region 标题栏后退键
             SystemNavigationManager systemNavigationManager = SystemNavigationManager.GetForCurrentView();
             systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
-
             if (_backPressedHandlers.ContainsKey(frame) == false)
             {
                 EventHandler<BackRequestedEventArgs> backRequestedHandler = (sender, e) =>
@@ -64,7 +95,9 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
                 _backRequestedHandlers.Add(frame, backRequestedHandler);
                 systemNavigationManager.BackRequested += backRequestedHandler;
             }
+            #endregion
 
+            #region 设备后退键
             if (HardwareButtonsHelper.IsUseable && _backPressedHandlers.ContainsKey(frame) == false)
             {
                 EventHandler<BackPressedEventArgs> backPressedHandler = (sender, e) =>
@@ -78,6 +111,28 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
                 _backPressedHandlers.Add(frame, backPressedHandler);
                 HardwareButtons.BackPressed += backPressedHandler;
             }
+            #endregion
+
+            #region 鼠标后退键
+            CoreWindow window = CoreWindow.GetForCurrentThread();
+            if (_frameWindows.ContainsKey(frame) == false && _pointerReleasedHandlers.ContainsKey(frame) == false)
+            {
+                TypedEventHandler<CoreWindow, PointerEventArgs> pointerReleasedHandler = (sender, e) =>
+                {
+                    e.Handled = true;
+                    if (e.CurrentPoint.Properties.PointerUpdateKind == PointerUpdateKind.XButton1Released)
+                    {
+                        if (action != null)
+                        {
+                            action.Invoke();
+                        }
+                    }
+                };
+                _frameWindows.Add(frame, window);
+                _pointerReleasedHandlers.Add(frame, pointerReleasedHandler);
+                window.PointerReleased += pointerReleasedHandler;
+            }
+            #endregion
         }
 
         public static void RegisterNavigateBack(this Frame frame)
@@ -94,22 +149,36 @@ namespace SoftwareKobo.UniversalToolkit.Helpers
 
         public static void UnregisterNavigateBack(this Frame frame)
         {
+            #region 标题栏后退键
             var systemNavigationManager = SystemNavigationManager.GetForCurrentView();
             systemNavigationManager.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Collapsed;
-
             if (_backRequestedHandlers.ContainsKey(frame))
             {
                 EventHandler<BackRequestedEventArgs> backRequestedHandler = _backRequestedHandlers[frame];
                 systemNavigationManager.BackRequested -= backRequestedHandler;
                 _backRequestedHandlers.Remove(frame);
             }
+            #endregion
 
+            #region 设备后退键
             if (HardwareButtonsHelper.IsUseable && _backPressedHandlers.ContainsKey(frame))
             {
                 EventHandler<BackPressedEventArgs> backPressedHandler = _backPressedHandlers[frame];
                 HardwareButtons.BackPressed -= backPressedHandler;
                 _backPressedHandlers.Remove(frame);
             }
+            #endregion
+
+            #region 鼠标后退键
+            if (_frameWindows.ContainsKey(frame) && _pointerReleasedHandlers.ContainsKey(frame))
+            {
+                CoreWindow window = _frameWindows[frame];
+                TypedEventHandler<CoreWindow, PointerEventArgs> pointerReleasedHandler = _pointerReleasedHandlers[frame];
+                window.PointerReleased -= pointerReleasedHandler;
+                _frameWindows.Remove(frame);
+                _pointerReleasedHandlers.Remove(frame);
+            }
+            #endregion
         }
     }
 }
